5f6d52da52d077e1403dbca61cbf7dec7f83565e
Simone Elizabeth Gardner <simonegdccc@gmail.com>
2025-11-09 15:03:17 -0500

    Implemented javascript for form

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
-/* Add your JavaScript to this file */
+const form = document.querySelector('.newsletter form');
+const emailInput = form.querySelector('input[type="email"]');
+const messageDiv = document.querySelector('.newsletter .message');
+form.addEventListener('submit', function(event) {
+    const email = emailInput.value;
+    if(email===''){
+        messageDiv.textContent = 'Please enter a valid email address.';
+    }else{
+        messageDiv.textContent = `Thank you! Your email address ${email} has been added to our mailing list!`;
+    }
+});
