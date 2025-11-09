6ad10a7d539575cf0e5ce967bf20daf95a96109b
Simone Elizabeth Gardner <simonegdccc@gmail.com>
2025-11-09 15:23:52 -0500

    added missing DOMContentLoaded

--- scripts.js
+++ scripts.js
@@ -1,12 +1,14 @@
-const form = document.querySelector('.newsletter form');
-const emailInput = form.querySelector('input[type="email"]');
-const messageDiv = document.querySelector('.newsletter .message');
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    const email = emailInput.value;
-    if(email===''){
-        messageDiv.textContent = 'Please enter a valid email address.';
-    }else{
-        messageDiv.textContent = `Thank you! Your email address ${email} has been added to our mailing list!`;
-    }
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('.newsletter form');
+    const emailInput = form.querySelector('input[type="email"]');
+    const messageDiv = document.querySelector('.newsletter .message');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const email = emailInput.value;
+        if(email===''){
+            messageDiv.textContent = 'Please enter a valid email address.';
+        }else{
+            messageDiv.textContent = `Thank you! Your email address ${email} has been added to our mailing list!`;
+        }
+    });
 });
